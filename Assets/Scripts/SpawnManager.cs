a017a131424a3a5608043bf3a1a435d1a7fc3baa
Zachary <159571194+JarJarSith0605@users.noreply.github.com>
2025-05-01 10:40:03 -0700

    animals get destroyed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,20 +5,20 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3, 1f);
     }
 
 public GameObject[] animalPrefabs;
 
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.S)) {
-         SpawnRandomAnimal();
-        }
+    void Update()
+    {
+
     }
+      
     void SpawnRandomAnimal(){
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
